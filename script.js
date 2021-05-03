2dd6d078987c7d301b3b3689153f925bf0d6beb0
Tung <31438811+VietTungFizzy@users.noreply.github.com>
2021-05-03 15:08:06 +0700

    modal for feeds

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 $(function() {
-	$(".feeds-page .like").click(function(e) {
-		
-	})
+
 })
 
 function like(data) {
-	if($(".feeds-page .like i").eq(data).hasClass("fas fa-heart text-blue-facebook")) {
-			$(".feeds-page .like i").eq(data).removeClass("fas fa-heart text-blue-facebook").addClass("far fa-heart text-blue-facebook")	
+	const iconElement = ".feeds-page #" + data + " i"
+	if($(iconElement).hasClass("fas fa-heart text-blue-facebook")) {
+			$(iconElement).removeClass("fas fa-heart text-blue-facebook").addClass("far fa-heart text-blue-facebook")	
 		}
 		else {
-			$(".feeds-page .like i").eq(data).removeClass("far fa-heart text-blue-facebook").addClass("fas fa-heart text-blue-facebook")	
+			$(iconElement).removeClass("far fa-heart text-blue-facebook").addClass("fas fa-heart text-blue-facebook")	
 		}
+}
+
+function toggleModal(data) {
+	console.log(data)
+	const image = $("#"+ data + " .card-img").attr("src")
+	const title = $("#"+ data + " .card-title.font-weight-bold").text()
+	const description = $("#"+ data + " .card-content").text()
+	$("#photoModal img").attr("src", image)
+	$("#photoModal #photoModalLabel").text(title)
+	$("#photoModal p").text(description)
+	$("#photoModal").modal("toggle")
 }
